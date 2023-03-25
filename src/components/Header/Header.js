5b9c1b0bd3c66c29b3a9cd117d4abb0c93b99385
Sanja <sanja.pistac@gmail.com>
2023-03-25 14:31:20 +0100

    Created main layout

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+
+function Header () {
+
+  return (
+    <div>
+      <a href="homepage">
+        <h1>Interviews Reports</h1>
+      </a>
+
+      <div>
+        <a href="homepage">
+          <h3>Candidates</h3>
+        </a>
+      </div>
+    </div>
+  )
+  
+}
+
+export default Header;
---
+++ src/components/MainContent/MainContent.js
@@ -0,0 +1,10 @@
+function MainContent () {
+
+  return (
+    <div className="main-content">
+      <h1>THis is where the magic begins...</h1>
+    </div>
+  )
+}
+
+export default MainContent;
