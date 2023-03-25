56853404721ad118d9a36670c486b07a6451060f
Sanja <sanja.pistac@gmail.com>
2023-03-25 17:23:15 +0100

    bpf-2 bpf-4, created candidates placeholders and cards

---
+++ src/components/CandidateCard/CandidateCard.js
@@ -0,0 +1,17 @@
+import "./CandidateCard.css";
+
+function CandidateCard () {
+
+  return (
+
+    <div className="card">
+      
+      <img src="" alt="A Marvel Hero" />
+      <h3>Candidate Name</h3>
+      <p>Email</p>
+    </div>
+  )
+
+}
+
+export default CandidateCard;
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,8 +1,45 @@
+import CandidatesPlaceholderCard from "../CandidatesPlaceholderCard/CandidatesPlaceholderCard";
+import CandidateCard from "../CandidateCard/CandidateCard";
+import "./MainContent.css"
+
 function MainContent () {
 
+  
+  let candidatesPlaceholderCard = [];
+  for (let i = 0; i < 8; i++) {
+    candidatesPlaceholderCard.push(null);
+  }
+
   return (
+
+    
     <div className="main-content">
-      <h1>THis is where the magic begins...</h1>
+      <h1>Candidates</h1>
+      <input type="text" placeholder="Search..." />
+      <hr></hr>
+      
+      <div className="candidates">
+      
+      {
+          
+          candidatesPlaceholderCard.map((item, index) => {
+            return (
+              <CandidatesPlaceholderCard /> 
+            )
+          })
+      }
+
+      {
+          
+            
+              candidates.map((item) => {
+                return (
+                  <CandidateCard />
+                )
+              })
+          }
+      
+      </div>
     </div>
   )
 }
