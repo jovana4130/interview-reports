c27f799804400acc10253312bb05ad5aaca61001
Nebojsa <nebojsaandric95@gmail.com>
2023-04-01 16:45:06 +0200

    Search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import "./App.css"
+import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
 //import CandidateReports from "./components/CandidateReports/CandidateReports";
 
 function App() {
-
   const [candidates, setCandidates] = useState([]);
   //const [users, setUsers] = useState([]);
 
-
-
   const fetchCandidates = () => {
-
     const url = "http://localhost:3333/api/candidates";
 
     fetch(url)
-      .then(data => data.json())
-      .then(res => {
+      .then((data) => data.json())
+      .then((res) => {
         setCandidates(res);
-      }
-      )
-  }
-
-
+      });
+  };
 
   /* const fetchUsers = () => {
      const url = "http://localhost:3333/api/users";
@@ -38,29 +31,24 @@ function App() {
    }*/
 
   useEffect(() => {
-
     fetchCandidates();
     //fetchUsers();
-
-  }, [])
-
-
+  }, []);
 
   return (
-
     <div className="App">
-
       <Header />
 
       <Routes>
-        <Route path="/candidates" element={<MainContent candidates={candidates} />} />
+        <Route
+          path="/candidates"
+          element={<MainContent candidates={candidates} />}
+        />
       </Routes>
 
-
-        
       <Footer />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,55 +1,58 @@
 import CandidatesPlaceholderCard from "../CandidatesPlaceholderCard/CandidatesPlaceholderCard";
 import CandidateCard from "../CandidateCard/CandidateCard";
+import { useState } from "react";
 
-import "./MainContent.css"
+import "./MainContent.css";
 
 function MainContent(props) {
-
   const candidates = props.candidates;
   //const users = props.users;
-
+  const [search, setSearch] = useState("");
 
   let candidatesPlaceholderCard = [];
   for (let i = 0; i < 7; i++) {
-    candidatesPlaceholderCard.push(null);
+    candidatesPlaceholderCard.push(i);
   }
+  const filteringCandidates = candidates.filter((item) => {
+    if (search.trim() === "") {
+      return true;
+    } else {
+      if (item.name.toUpperCase().includes(search.toUpperCase())) {
+        return true;
+      }
+      return false;
+    }
+  });
 
   return (
-
-
     <div className="main-content">
-
       <div className="candidates">
         <h1>Candidates</h1>
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          name="search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
       </div>
       <hr></hr>
 
       <div className="candidates-list">
-
-        {
-          candidates.length === 0 &&
+        {candidates.length === 0 &&
           candidatesPlaceholderCard.map((item, index) => {
-            return (
-              <CandidatesPlaceholderCard key={index} />
-            )
-          })
-        }
+            return <CandidatesPlaceholderCard key={index} />;
+          })}
 
-        {
-
-          candidates.length > 0 &&
-          candidates.map((item) => {
-            return (
-              <CandidateCard key={item.id} item={item} />
-
-            )
-          })
-        }
+        {candidates.length > 0 &&
+          filteringCandidates.map((item) => {
+            return <CandidateCard key={item.id} item={item} />;
+          })}
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default MainContent;
