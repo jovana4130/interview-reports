2fc1f309d4e8d76262edf5087c13ca15073d5c69
Gradimir <gradimirbabic2@gmail.com>
2023-04-01 16:32:43 +0200

    Candidate report

--- src/components/CandidateCard/CandidateCard.js
+++ src/components/CandidateCard/CandidateCard.js
@@ -1,26 +1,30 @@
 import "./CandidateCard.css";
+import {useNavigate} from 'react-router-dom'
 
-function CandidateCard(props) {
 
-  const item = props.item;
-  console.log(item);
-  const imgSrc = item.avatar;
-
+function CandidateCard({candidate}) {
+  const navigate = useNavigate()
+  
+ 
+  
 
   return (
-
-    <div className="card">
+    //()=> navigate('/candidates/'+`${id}`)
+    <div>
+      <div className="card" onClick={()=> navigate('/candidate/'+candidate.id)}>
 
       <div className="candidate-image">
-        <img src={imgSrc} alt="A Candidate" />
+        <img src={candidate.avatar} alt="A Candidate" />
       </div>
 
       <div className="candidate-info">
-        <h3>{item.name}</h3>
-        <p>{item.email}</p>
+        <h3>{candidate.name}</h3>
+        <p>{candidate.email}</p>
+      </div> 
       </div>
-
+    {/*<Outlet/>*/} 
     </div>
+    
   )
 
 }
--- src/components/CandidateReports/CandidateReports.js
+++ src/components/CandidateReports/CandidateReports.js
@@ -1,17 +1,95 @@
-
-import { useParams } from "react-router"
+ 
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 import "./CandidateReports.css"
+import {TfiEye} from 'react-icons/tfi';
+import InterviewReportModal from "../InterviewReportModal/InterviewReportModal";
 
-function CandidateReports () {
-
+function CandidateReports() {
   const {id} = useParams(); 
+  const [candidate,setCandidate] = useState([])
+  const [candidateReports,setCandidateReports] = useState([])
+  const [modalOpened, setModalOpened] = useState(false);
+  const [selectedReport, setSelectedReport] = useState([]);
+  const candidateFetch = (id) =>{
+    const url = 'http://localhost:3333/api/candidates?id='+id;
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+      if(data[0] && data[0].id){
+        console.log(data);
+        setCandidate(data[0]);
+      }
+    })
+  }
+
+  const candidateReportsFetch = (id) =>{
+    const url = 'http://localhost:3333/api/reports?candidateId='+id;
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>{
+      if(data && Array.isArray(data)){
+        setCandidateReports(data);
+      }
+    })
+  }
   
+  useEffect(()=>{
+    candidateFetch(id)
+    candidateReportsFetch(id)
+  },[id])
+
+  const _handleClickView =(report)=>{
+    setModalOpened(true);
+    setSelectedReport(report);
+
+  }
+
+  const closeModal =()=>{
+    setModalOpened(false);
+    setSelectedReport(null);
+  }
+
+  console.log(candidate);
   return (
     <div className="candidate-reports">
-      <h1>Candidate Reports</h1>
+      <div onClick={CandidateReports}></div>
+      <p>Name:</p>
+      <p>{candidate.name}</p>
+      <p>Date of birth:</p>
+      <p>{candidate.birthday}</p>
+      <p>Email:</p>  
+      <p>{candidate.email}</p>
+      <p>Education:</p>
+      <p>{candidate.education}</p>
+  
+      <p>Reports</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Interview Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {candidateReports.map((item)=>{
+            const dateString = `${item.interviewDate}`;
+            const date = new Date(dateString);
+            const formattedDate = date.toLocaleString();
+            return (
+              <tr key={item.id}>
+                <td>{item.companyName}</td>
+                <td>{formattedDate}</td>
+                <td>{item.status}</td>
+                <td><span onClick={(e)=>{_handleClickView(item)}}><TfiEye/></span></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      {modalOpened && (<InterviewReportModal closeModal={closeModal} selectedReport={selectedReport}/>)}
 
-      <h2> Candidate</h2>
-      <p>Candidates Id: {id}</p>
     </div>
   )
 }
